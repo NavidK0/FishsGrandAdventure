39818279c0ae49b1e2aa838828b7de3e83e46202
Navid Kabir <navid113@gmail.com>
2023-12-14 22:47:18 -0800

    Increase Bracken spawn rate on Wendigo event

--- Game/Events/WendigoEvent.cs
+++ Game/Events/WendigoEvent.cs
@@ -1,4 +1,7 @@
-﻿using FishsGrandAdventure.Network;
+﻿using System;
+using System.Collections.Generic;
+using FishsGrandAdventure.Network;
+using FishsGrandAdventure.Utils;
 using UnityEngine;
 
 namespace FishsGrandAdventure.Game.Events;
@@ -8,6 +11,20 @@ public class WendigoEvent : BaseGameEvent
     public override string Description => "The Wendigo";
     public override Color Color => new Color(0.2f, 0.07f, 0.09f);
     public override GameEventType GameEventType => GameEventType.Wendigo;
+
+    public override void OnPreModifyLevel(ref SelectableLevel level)
+    {
+        ModUtils.AddSpecificEnemiesForEvent(level, new List<Type> { typeof(FlowermanAI) });
+
+        foreach (SpawnableEnemyWithRarity spawnableEnemyWithRarity in level.Enemies)
+        {
+            if (spawnableEnemyWithRarity.enemyType.enemyPrefab.GetComponent<FlowermanAI>() != null)
+            {
+                spawnableEnemyWithRarity.rarity = 100;
+                spawnableEnemyWithRarity.enemyType.MaxCount = 4;
+            }
+        }
+    }
 
     public override void OnPostFinishGeneratingLevel()
     {
